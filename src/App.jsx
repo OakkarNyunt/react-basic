bd777fb437d240b68d2ebe24e0e91bb6ba70145a
Oakkar1998 <oakkarnyunt@gmail.com>
2025-07-26 15:46:40 +0630

    state counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// import { useState } from 'react'
+import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -6,19 +6,15 @@ import Home from './components/home.jsx'
 import Button from './components/button.jsx'
 
 function App() {
-  // const [count, setCount] = useState(0)
-  const user = { name: 'Junior React Developer', age: 23 }
-  const products = [ "Laptop", "Mobile", "Tablet" ]
-  const onClickHandler = () =>{
-    alert("Button clicked!");
-  }
+  const [user, setUser] = useState({name:true, age: true, isbusy: true})
+
+  
   return (
     <>
-     <Home {...user}/>
-     <Button btn= "submit" onPress = {onClickHandler}/>
-     {products.map((product)=>(
-      <p key={products.index}>{product}</p>
-     ))}
+      <p>Name:{ user.name ? "oakkar" : "Shunn Lae Kay Khaing" }</p>
+      <p>Age: {user.name ? 27 : 22 }</p>
+      <p>Status: {user.isbusy ? 'Busy' : 'Available'}</p>
+      <button onClick={() => setUser((previous) => ({age : !previous.age,name : !previous.name,  isbusy: !previous.isbusy}))}>Change Status</button>
     </>
   )
 }
