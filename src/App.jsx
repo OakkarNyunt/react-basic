7168704392d8e7548975409b0dc3fc6491a83e07
Oakkar1998 <oakkarnyunt@gmail.com>
2025-07-26 19:18:54 +0630

    finish React Basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,7 +7,20 @@ import Button from './components/button.jsx'
 
 function App() {
   const [user, setUser] = useState({name:true, age: true, isbusy: true})
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+  const [btn,setBtn] = useState("");
 
+  useEffect( () =>{
+    if(!name){
+      setError("Please enter your name")
+      setBtn('')
+    }else{
+      setError("")
+      setBtn(<button>Click</button>)
+    }
+  },[name]
+)
   
   return (
     <>
@@ -15,6 +28,14 @@ function App() {
       <p>Age: {user.name ? 27 : 22 }</p>
       <p>Status: {user.isbusy ? 'Busy' : 'Available'}</p>
       <button onClick={() => setUser((previous) => ({age : !previous.age,name : !previous.name,  isbusy: !previous.isbusy}))}>Change Status</button>
+      <form>
+        <p>Enter your name:{name}</p>
+        
+        
+        <input type="text" value={name} onChange={(e)=> setName(e.target.value.trim())} />
+        {btn}
+        <p>{error}</p>
+      </form>
     </>
   )
 }
